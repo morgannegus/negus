d85a0227f617ee29b484beeb3d3a063abfa341f5
morgannegus <115803961+morgannegus@users.noreply.github.com>
2022-11-04 14:23:25 +0000

    added response button for "How are you?"

--- script.js
+++ script.js
@@ -6,11 +6,48 @@
 function submitForm(name, hometown) {
   // search the DOM for a specfic tag with the id "guestlist"
   var guestlist = document.getElementById("guestlist");
-  var name = document.getElementById("name");
-  var hometown = document.getElementById("hometown");
+  var n = document.getElementById("name");
+  var h = document.getElementById("hometown");
+  var currentguestlist = localStorage.getItem("guestlist");
+  localStorage.setItem("guestlist", currentguestlist + "<li>" + name + ". " + hometown + "</li>");
   guestlist.innerHTML += "<li>" + name + ". " + hometown + "</li>";
   n.value = "";
   n.value = "";
   n.focus();
+  return false;
+}
+
+function clearStorageGB() {
+  var guestlist = document.getElementById("guestlist");
+  guestlist.innerHTML = "";
+  localStorage.setItem("guestlist","");
+}
+
+function checkStorageGB() {
+  var guestlist = document.getElementById("guestlist");
+  var currentguestlist = localStorage.getItem("guestlist");
+  guestlistlist.innerHTML = currentguestlist;
+}
+
+function clearStorage() {
+  var answerlist = document.getElementById("answerlist");
+  answerlist.innerHTML = "";
+  localStorage.setItem("answerlist","");
+}
+
+function checkStorage() {
+  var answerlist = document.getElementById("answerlist");
+  var currentanswers = localStorage.getItem("answerlist");
+  answerlist.innerHTML = currentanswers;
+}
+
+function submitAnswer(answer) {
+  // search the DOM for a specfic tag with the id "answerlist"
+  var answerlist = document.getElementById("answerlist");
+  var currentanswers = localStorage.getItem("answerlist");
+  localStorage.setItem("answerlist", currentanswers + "<br />" + answer.value);
+  answerlist.innerHTML += "<br />" + answer.value;
+  answer.value = "";
+  answer.focus();
   return false;
 }
